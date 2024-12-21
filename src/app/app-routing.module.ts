c8e6f4b1ec663e87e3a490fb4baee0b2a60bb9ce
Samir Temtem <samir.temtem@esprit.tn>
2024-12-21 11:09:05 +0100

    Last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { ParticipantsModule } from './participants/participants.module';
+const routes: Routes = [
+  { path: '', redirectTo: '/teams', pathMatch: 'full' },
+  { path: 'teams', loadChildren: () => import('./team/team.module').then(m => m.TeamModule) },
+  { path : 'participants', loadChildren: () => import('./participants/participants.module').then(m => m.ParticipantsModule) },
+  { path: '**', component: NotFoundComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavBarComponentComponent } from './nav-bar-component/nav-bar-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponentComponent,
+    FooterComponentComponent,
+    NotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
